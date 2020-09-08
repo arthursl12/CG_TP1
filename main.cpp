1ef2501b849ed8203889c832f5c8eaec8d1b6006
arthursl12 <arthursoutolima@gmail.com>
2020-09-08 13:17:23 -0300

    Simple Pong Game

--- main.cpp
+++ main.cpp
@@ -1,80 +1,209 @@
-#include "GL/gl.h"
-#include "GL/glut.h"
+#include "GL/freeglut.h"
+#include <iostream>
+#include <string>
+#include <cmath>
 
-#include "game.h"
+// keycodes
+#define VK_W 0x57
+#define VK_S 0x53
 
-#define COLUMNS 40
-#define ROWS 40
-#define FPS 10
+// window size and update rate (60 fps)
+int width = 500;
+int height = 200;
+int interval = 1000 / 60;
 
-extern short sDirection;
+// score
+int score_left = 0;
+int score_right = 0;
 
-void display_callback();
-void reshape_callback(int,int);
-void timer_callback(int);
-void keyboard_callback(int, int, int);
+// rackets in general
+int racket_width = 10;
+int racket_height = 80;
+int racket_speed = 3;
 
-void init(){
-	glClearColor(0,0,0,1);
-	initGrid(COLUMNS, ROWS);
+// left racket position
+float racket_left_x = 10.0f;
+float racket_left_y = 50.0f;
+
+// right racket position
+float racket_right_x = width - racket_width - 10;
+float racket_right_y = 50;
+
+// ball
+float ball_pos_x = width / 2;
+float ball_pos_y = height / 2;
+float ball_dir_x = -1.0f;
+float ball_dir_y = 0.0f;
+int ball_size = 8;
+int ball_speed = 2;
+
+
+void keyboard_callback(unsigned char, int, int);
+
+void drawText(float x, float y, std::string text) {
+    glRasterPos2f(x, y);
+    glutBitmapString(GLUT_BITMAP_8_BY_13, (const unsigned char*)text.c_str());
+}
+
+void drawRect(float x, float y, float width, float height) {
+    glBegin(GL_QUADS);
+        glVertex2f(x, y);
+        glVertex2f(x + width, y);
+        glVertex2f(x + width, y + height);
+        glVertex2f(x, y + height);
+    glEnd();
+}
+
+void draw() {
+    // clear (has to be done at the beginning)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    // ToDo: draw our scene
+	// draw rackets
+    drawRect(racket_left_x, racket_left_y, racket_width, racket_height);
+    drawRect(racket_right_x, racket_right_y, racket_width, racket_height);
+
+	// draw score
+    drawText(width / 2 - 10, height - 15, std::to_string(score_left) + ":" + std::to_string(score_right));
+	
+	// draw ball
+	drawRect(ball_pos_x - ball_size / 2, ball_pos_y - ball_size / 2, ball_size, ball_size);
+
+    // swap buffers (has to be done at the end)
+    glutSwapBuffers();
+}
+
+void vec2_norm(float& x, float &y) {
+    // sets a vectors length to 1 (which means that x + y == 1)
+    float length = sqrt((x * x) + (y * y));
+    if (length != 0.0f) {
+        length = 1.0f / length;
+        x *= length;
+        y *= length;
+    }
+}
+
+void updateBall() {
+    // fly a bit
+    ball_pos_x += ball_dir_x * ball_speed;
+    ball_pos_y += ball_dir_y * ball_speed;
+    
+    // hit by left racket?
+    if (ball_pos_x < racket_left_x + racket_width &&
+        ball_pos_x > racket_left_x &&
+        ball_pos_y < racket_left_y + racket_height &&
+        ball_pos_y > racket_left_y) {
+        // set fly direction depending on where it hit the racket
+        // (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
+        float t = ((ball_pos_y - racket_left_y) / racket_height) - 0.5f;
+        ball_dir_x = fabs(ball_dir_x); // force it to be positive
+        ball_dir_y = t;
+    }
+   
+    // hit by right racket?
+    if (ball_pos_x > racket_right_x &&
+        ball_pos_x < racket_right_x + racket_width &&
+        ball_pos_y < racket_right_y + racket_height &&
+        ball_pos_y > racket_right_y) {
+        // set fly direction depending on where it hit the racket
+        // (t is 0.5 if hit at top, 0 at center, -0.5 at bottom)
+        float t = ((ball_pos_y - racket_right_y) / racket_height) - 0.5f;
+        ball_dir_x = -fabs(ball_dir_x); // force it to be negative
+        ball_dir_y = t;
+    }
+
+    // hit left wall?
+    if (ball_pos_x < 0) {
+        ++score_right;
+        ball_pos_x = width / 2;
+        ball_pos_y = height / 2;
+        ball_dir_x = fabs(ball_dir_x); // force it to be positive
+        ball_dir_y = 0;
+    }
+
+    // hit right wall?
+    if (ball_pos_x > width) {
+        ++score_left;
+        ball_pos_x = width / 2;
+        ball_pos_y = height / 2;
+        ball_dir_x = -fabs(ball_dir_x); // force it to be negative
+        ball_dir_y = 0;
+    }
+
+    // hit top wall?
+    if (ball_pos_y > height) {
+        ball_dir_y = -fabs(ball_dir_y); // force it to be negative
+    }
+
+    // hit bottom wall?
+    if (ball_pos_y < 0) {
+        ball_dir_y = fabs(ball_dir_y); // force it to be positive
+    }
+
+    // make sure that length of dir stays at 1
+    vec2_norm(ball_dir_x, ball_dir_y);
 }
 
 
-int main(int argc, char **argv){
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowSize(500,500);
-	glutCreateWindow("SNAKE");
-	glutDisplayFunc(display_callback);
-	glutReshapeFunc(reshape_callback);
-	glutTimerFunc(0,timer_callback,0);
-	glutSpecialFunc(keyboard_callback);
-	init();
-	glutMainLoop();
-	return 0;
+void update(int value) {
 
+   // update ball
+   updateBall();
+
+   // Call update() again in 'interval' milliseconds
+   glutTimerFunc(interval, update, 0);
+	
+
+   // Redisplay frame
+   glutPostRedisplay();
 }
 
-int index = 0;
-void display_callback(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	drawGrid();
-	drawSnake();
-	glutSwapBuffers();
-
+void enable2D(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, width, 0.0f, height, 0.0f, 1.0f);
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity();
 }
 
-void reshape_callback(int w, int h){
-	glViewport(0,0,(GLsizei)w,(GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0,COLUMNS,0,ROWS,-1,1);
-	glMatrixMode(GL_MODELVIEW);
+int main(int argc, char** argv) {
+    // initialize opengl (via glut)
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("noobtuts.com Pong");
+    glutKeyboardFunc(keyboard_callback);
+
+    // Register callback functions  
+    glutDisplayFunc(draw);
+    glutTimerFunc(interval, update, 0);
+
+    // setup scene to 2d mode and set draw color to white
+    enable2D(width, height);
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    // start the whole thing
+    glutMainLoop();
+    return 0;
 }
 
-void keyboard_callback(int key, int, int){
+void keyboard_callback(unsigned char key, int, int){
 	switch(key){
-		case GLUT_KEY_UP:
-			if(sDirection != DOWN)
-				sDirection = UP;
+		case 'w':
+			racket_left_y += racket_speed;
 			break;
-		case GLUT_KEY_DOWN:
-			if(sDirection != UP)
-				sDirection = DOWN;
+		case 's':
+			racket_left_y -= racket_speed;
 			break;
-		case GLUT_KEY_RIGHT:
-			if(sDirection != LEFT)
-				sDirection = RIGHT;
+		case 't':
+			racket_right_y += racket_speed;
 			break;
-		case GLUT_KEY_LEFT:
-			if(sDirection != RIGHT)
-				sDirection = LEFT;
+		case 'g':
+			racket_right_y -= racket_speed;
 			break;
 	}
 }
 
-void timer_callback(int){
-	glutPostRedisplay();
-	glutTimerFunc(1000/FPS,timer_callback,0);
-}
 
